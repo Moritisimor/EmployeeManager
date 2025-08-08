9bf6ea30efb143b496ac0951ab072da0e2590cbf
Moritisimor <devMoritisimor@proton.me>
2025-08-08 11:57:56 +0200

    Added the ability to create new employees graphically. The other pages are WIP

---
+++ GoHostingServer/Site/EmployeeCreation/EmployeeCreation.js
@@ -0,0 +1,30 @@
+let firstNameTextArea       = document.getElementById("firstNameTextArea");
+let lastNameTextArea        = document.getElementById("lastNameTextArea");        
+let positionTextArea        = document.getElementById("positionTextArea");
+let idTextArea              = document.getElementById("idTextArea");
+let monthlySalaryTextArea   = document.getElementById("monthlySalaryTextArea");
+let submitButton            = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", () => {
+    let path = `/EmployeeCreate/${firstNameTextArea.value}/${lastNameTextArea.value}/${positionTextArea.value}
+    /${idTextArea.value}/${monthlySalaryTextArea.value}`;
+
+    fetch("http://localhost:8070" + path)
+    .then(response => {
+        if (!response.ok) {
+            if (response.status == 400) {
+                alert("Invalid input! Monthly salary must be of numeric value!");
+                throw new error("Bad Request error");
+            } else if (response.status == 409) {
+                alert("This ID is already taken! Try another one.");
+                throw new error("Conflict error");
+            } else {
+                alert("An unknown error has occurred!");
+                throw new error("Unknown error");
+            }
+        }
+        if (response.status == 201) {
+            alert("Succesfully created employee!");
+        }
+    })
+});
